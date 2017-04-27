788e9b0fac1e79d27e7367d104003f233900de85
[MartinBear] <[bhluum@gmail.com]>
2017-04-27 16:41:46 +0200

    Added Module,namespacing,invoking functions and cleaned up the code

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+const norris = (() =>{
+
+return {
+
+/* 
+The display function is not much code but it helps me
+keeping it DRY.
+*/
+
+display: (joke) => {
+  joke = joke.value.joke;
+  $('.joke').html(joke);
+},
+
 /*
 Fetching a random joke form ICNDB http://api.icndb.com.
 The fetchJoke function makes a 'GET' request with the ajax method and
@@ -6,76 +19,79 @@ combined with a error catcher as a failsafe.
 The success function has 1 parameter, joke(data),
 the values will be displayed with the JQuerys equivalent to JavaScript "document.getElement.." 
 */
-function fetchJoke() {
-	let url = 'http://api.icndb.com/jokes/random?';
-	$.ajax({
-  	dataType: "json",
-  	url: url,
- 	success: function(joke) {
-  		joke = joke.value.joke;
-  		$('.joke').html(joke); 
-  		return (joke);
-  	},
-  	error: function() {
-  		alert('Shit has hit the fan, boss')
-  	}
+
+fetchJoke: () => {
+  let url = 'http://api.icndb.com/jokes/random?';
+  $.ajax({
+    method: 'GET',
+    dataType: "json",
+    url: url,
+  success: function(joke) {
+    norris.display(joke);
+    },
+    error: function() {
+      alert('Shit has hit the fan, boss')
+      console.log(joke);
+    }
 });
-}
+},
 
-function category() {
-	let url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]';
-	$.ajax({
-  	dataType: "json",
-  	url: url,
- 	success: function(joke) {	
-  		joke = joke.value.joke;
-  		$('.joke').html(joke); 
-  		return (joke);
-  	},
-  	error: function() {
-  		alert('Shit has hit the fan, boss')
-  	}
-});	
-}
-
+category: () => {
+  let url = 'http://api.icndb.com/jokes/random?limitTo=[nerdy]';
+  $.ajax({
+    method: 'GET',
+    dataType: "json",
+    url: url,
+  success: function(joke) { 
+    norris.display(joke);
+    },
+    error: function() {
+      alert('Shit has hit the fan, boss')
+      console.log(joke);
+    }
+}); 
+},
 /* 
 This function stores the given names in "first" and "last".
 next step is to chopp the URL and use the first&last name variables.
 The values will be transfered from the inputform
-
 */
-function yourName () {
-	let first = $('#firstName').val();
-  	let last = $('#lastName').val();
-	let url = 'http://api.icndb.com/jokes/random?firstName=' + first + '&lastName='+ last + '&amp;json&callback=?';
-	$.ajax({
-  	dataType: "json",
-  	url: url,
- 	success: function(joke) {	
-  		joke = joke.value.joke;
-  		$('.joke').html(joke); 
-  		return (joke);
-  	},
-  	error: function() {
-  		alert('Shit has hit the fan, boss')
-  	}
+yourName: () => {
+  let first = $('#firstName').val();
+  let last = $('#lastName').val();
+  let url = 'http://api.icndb.com/jokes/random?firstName=' + first + '&lastName='+ last + '&amp;json&callback=?';
+  $.ajax({
+    method: 'GET',
+    dataType: "json",
+    url: url,
+  success: function(joke) { 
+    norris.display(joke);
+    },
+    error: function() {
+      alert('Shit has hit the fan, boss')
+      console.log(joke);
+    }
 });
-}
+},
+/*
+an invoking function that renders a joke on start up 
+and handeling all the html elements.
+*/
+init: () => {
+  norris.fetchJoke();
+  $('.newJoke').on('click', norris.fetchJoke);
+  $('.nerdyBtn').on('click', norris.category);
+  $('.nameBtn').on('click', norris.yourName);
+  },
 
-//Fetching a joke on start up
-  fetchJoke();
 
-  $(".newJoke").on("click", function() {
-    fetchJoke();
-});
-  $(".nerdyBtn").on("click", function() {
-    category();
-});
-    $(".nameBtn").on("click", function() {
-    yourName();
-});
+}; //return
 
-});
+
+})();
+norris.init();
+
+
 // http://api.icndb.com/jokes/random?firstName=#{firstName}&lastName=#{lastName}&limitTo=[nerdy]")
 // http://api.icndb.com/jokes/random?limitTo=[nerdy]
 // http://api.icndb.com/jokes/random?limitTo=[explicit]
